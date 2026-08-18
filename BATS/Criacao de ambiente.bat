814d903f380d7107cc54d9d2ab2ceb287448129f
jeferson-junior-dev <jeferson.j.junior@ba.estudante.senai.br>
2026-08-18 20:38:01 -0300

    feat: first commit com lote de criacao de ambiente

---
+++ BATS/Criacao de ambiente.bat
@@ -0,0 +1,15 @@
+@echo off
+title Preparação do ambiente de backup
+
+mkdir "C:\Users\232.005495\Documents\AulaLote\Implementação\Teste_Implementacao\scripts" 2>nul
+mkdir "C:\Users\232.005495\Documents\AulaLote\Implementação\Teste_Implementacao\dados" 2>nul
+mkdir "C:\Users\232.005495\Documents\AulaLote\Implementação\Teste_Implementacao\backups\arquivos" 2>nul
+mkdir "C:\Users\232.005495\Documents\AulaLote\Implementação\Teste_Implementacao\backups\fisico" 2>nul
+mkdir "C:\Users\232.005495\Documents\AulaLote\Implementação\Teste_Implementacao\backups\tabelas" 2>nul
+mkdir "C:\Users\232.005495\Documents\AulaLote\Implementação\Teste_Implementacao\backups\completo" 2>nul
+mkdir "C:\Users\232.005495\Documents\AulaLote\Implementação\Teste_Implementacao\logs" 2>nul
+mkdir "C:\Users\232.005495\Documents\AulaLote\Implementação\Teste_Implementacao\restauracao" 2>nul
+
+echo Estrutura criada com sucesso
+
+pause
